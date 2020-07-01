a92e7df2179fb4ccdbcd4250a029f36d994031cd
Vinícius Fantaccini <>
2020-07-01 18:19:28 -0300

    Primeiro layout criado

--- app/src/main/java/thefantaccini/agenda/MainActivity.java
+++ app/src/main/java/thefantaccini/agenda/MainActivity.java
@@ -2,6 +2,16 @@ package thefantaccini.agenda;
 
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.android.justjava.R;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
