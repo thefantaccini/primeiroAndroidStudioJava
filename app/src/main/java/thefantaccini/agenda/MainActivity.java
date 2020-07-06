1e6819ca996b321e9e6c6093f84bf1a31459c02e
Vinícius Fantaccini <>
2020-07-06 12:20:11 -0300

    Atualização 6/6 12h20

--- app/src/main/java/thefantaccini/agenda/MainActivity.java
+++ app/src/main/java/thefantaccini/agenda/MainActivity.java
@@ -3,15 +3,29 @@ package thefantaccini.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.example.android.justjava.R;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "Jose"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
